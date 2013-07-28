3e8e5c08dbdce9b29873fb58da452efa354bbda3
Jijo john <jijojohn32@gmail.com>
2013-07-28 18:36:38 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+*Configuration file
+*Coded by jijojohn @ singlelinelogics.com
+*/
+
+define("website","http://localhost/ms/"); //Application installed path
+define("mysql_Host","localhost"); //Host
+define("db_Name","angel"); //Dbname
+define("db_Username","root"); //Db Username
+define("db_Password",""); //Db password
+define("Upload_dir","../images/upload/"); //upload folder
+?>
